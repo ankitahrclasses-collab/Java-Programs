d56cfe33cfe6c671b915360d0c5ca72103164b86
ankitahrclasses-collab <ankita.hrclasses@gmail.com>
2025-08-26 13:44:52 +0530

    Add files via upload

---
+++ Boo.java
@@ -0,0 +1,14 @@
+class Boo {
+    public static void main(String []args){
+        int a=5;
+        int b=-5;
+
+        if(b>0){
+            System.out.println("The number is positive");
+        }
+        else{
+            System.out.println("The number is negative");
+        }
+    }
+    
+}
